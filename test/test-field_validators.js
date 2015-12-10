b10870cfd57af9971b8749b4cc1b191aef0e47a0
jhsware <sebastian@urbantalk.se>
2015-12-10 13:54:31 +0100

    Added some tests for DateField

--- test/test-field_validators.js
+++ test/test-field_validators.js
@@ -398,4 +398,31 @@ describe('Field validators', function() {
             expect(tmp).to.not.be(undefined);
         });
     });
+    
+    describe('Date field', function() {
+        it('accepts strings', function() {        
+            var dateField = validators.dateField({required: true});
+        
+            var tmp = dateField.validate("2015-01-01");
+            expect(tmp).to.be(undefined);
+        });
+    
+        it('throws error on undefined if required', function() {        
+            var dateField = validators.dateField({required: true});
+            var tmp = dateField.validate();
+            expect(tmp).to.not.be(undefined);
+        });
+        
+        it('throws error on malformed date', function() {        
+            var dateField = validators.dateField({required: false});
+            var tmp = dateField.validate("2015-1-9");
+            expect(tmp).to.not.be(undefined);
+        });
+
+        it('throws error on invalid date', function() {        
+            var dateField = validators.dateField({required: false});
+            var tmp = dateField.validate("2015-13-01");
+            expect(tmp).to.not.be(undefined);
+        });
+    });
 });
