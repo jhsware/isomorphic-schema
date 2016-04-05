304c90a9fdaa77471fb1aceec0d7efe74c070423
jhsware <sebastian@urbantalk.se>
2016-04-05 13:01:24 +0200

    Allow specifying an interface instead of schema on objectField

--- test/test-field_validators.js
+++ test/test-field_validators.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var expect = require('expect.js');
 
 var validators = require('../lib/field_validators');
+var Schema = require('../lib/schema');
 
 describe('Field validators', function() {
     describe('Base field', function() {
@@ -564,5 +565,93 @@ describe('Field validators', function() {
         });        
         
     });
+    
+    describe('Object field with defined schema', function() {
+        var objectSchema = new Schema("User Schema", {
+            username: validators.textField({required: true}),
+        })
+        
+        it('accepts valid object', function() {
+            var objectField = validators.objectField({
+                required: true,
+                schema: objectSchema
+            });
+        
+            var tmp = objectField.validate({
+                username: "test_user"
+            });
+            
+            expect(tmp).to.be(undefined);
+        });
+    
+        it('throws error on undefined if required', function() {        
+            var objectField = validators.objectField({
+                required: true,
+                schema: objectSchema
+            });
+            
+            var tmp = objectField.validate();
+            expect(tmp).to.not.be(undefined);
+        });
+
+        it("throws error when object schema doesn't validate", function() {        
+            var objectField =  validators.objectField({
+                required: true,
+                schema: objectSchema
+            });
+            
+            var tmp = objectField.validate({
+                username: undefined
+            });
+            expect(tmp).to.not.be(undefined);
+        });        
+        
+    });
+
+    describe('Object field with defined interface', function() {
+        var objectSchema = new Schema("User Schema", {
+            username: validators.textField({required: true}),
+        })
+        var fakeInterface = {
+            schema: objectSchema
+        };
+        
+        it('accepts valid object', function() {
+            var objectField = validators.objectField({
+                required: true,
+                interface: fakeInterface
+            });
+        
+            var tmp = objectField.validate({
+                username: "test_user"
+            });
+            
+            expect(tmp).to.be(undefined);
+        });
+    
+        it('throws error on undefined if required', function() {        
+            var objectField = validators.objectField({
+                required: true,
+                interface: fakeInterface
+            });
+            
+            var tmp = objectField.validate();
+            expect(tmp).to.not.be(undefined);
+        });
+
+        it("throws error when object schema doesn't validate", function() {        
+            var objectField =  validators.objectField({
+                required: true,
+                interface: fakeInterface
+            });
+            
+            var tmp = objectField.validate({
+                username: undefined
+            });
+            expect(tmp).to.not.be(undefined);
+        });        
+        
+    });
+
     
 });
