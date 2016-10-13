8b234fc933924ab4563d84279a9eac7dc682f066
jhsware <sebastian@urbantalk.se>
2016-10-13 19:48:21 +0200

    Added min and max length to TextArea field

--- test/test-field_validators.js
+++ test/test-field_validators.js
@@ -37,7 +37,7 @@ describe('Field validators', function() {
         });
     });
 
-    describe('String field', function() {
+    describe('Text field', function() {
         it('accepts strings', function() {        
             var textField = validators.textField({required: true});
         
@@ -87,47 +87,47 @@ describe('Field validators', function() {
     });
     
     
-    describe('String field', function() {
+    describe('TextArea field', function() {
         it('accepts strings', function() {        
-            var textField = validators.textField({required: true});
+            var textField = validators.textAreaField({required: true});
         
             var tmp = textField.validate("this is a sting");
             expect(tmp).to.be(undefined);
         });
     
         it('throws error on undefined if required', function() {        
-            var textField = validators.textField({required: true});
+            var textField = validators.textAreaField({required: true});
             var tmp = textField.validate();
             expect(tmp).to.not.be(undefined);
         });
 
         it('throws error on integer', function() {        
-            var textField = validators.textField({required: false});
+            var textField = validators.textAreaField({required: false});
             var tmp = textField.validate(4);
             expect(tmp).to.not.be(undefined);
         });
         
         it('throws error if text is longer than maxLength', function() {        
-            var textField = validators.textField({maxLength: 5});
+            var textField = validators.textAreaField({maxLength: 5});
             var tmp = textField.validate("123456");
             expect(tmp).to.not.be(undefined);
         });
         
         it('throws error if text is shorter than minLength', function() {        
-            var textField = validators.textField({minLength: 5});
+            var textField = validators.textAreaField({minLength: 5});
             var tmp = textField.validate("1234");
             expect(tmp).to.not.be(undefined);
         });
         
         it('accepts if length is at bottom end inbetween max- and minLength', function() {        
-            var textField = validators.textField({minLength: 3, maxLength: 10});
+            var textField = validators.textAreaField({minLength: 3, maxLength: 10});
         
             var tmp = textField.validate("123");
             expect(tmp).to.be(undefined);
         });
         
         it('accepts if length is at top end inbetween max- and minLength', function() {        
-            var textField = validators.textField({minLength: 3, maxLength: 10});
+            var textField = validators.textAreaField({minLength: 3, maxLength: 10});
         
             var tmp = textField.validate("1234567890");
             expect(tmp).to.be(undefined);
