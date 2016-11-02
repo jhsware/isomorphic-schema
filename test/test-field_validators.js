2d288237c23060ee1cd3646d5d6ff52042506c73
jhsware <sebastian@urbantalk.se>
2016-11-02 11:55:24 +0100

    Added general phone number field

--- test/test-field_validators.js
+++ test/test-field_validators.js
@@ -790,5 +790,39 @@ describe('Field validators', function() {
         
     });
 
+    describe('Telephone field', function() {
+        it('accepts international phonenumber', function() {        
+            var phoneField = validators.telephoneField({required: true});
+        
+            var tmp = phoneField.validate("+46 707 555 555");
+            expect(tmp).to.be(undefined);
+        });
+
+        it('accepts local phonenumber', function() {        
+            var phoneField = validators.telephoneField({required: true});
+        
+            var tmp = phoneField.validate("0707-55 55 55");
+            expect(tmp).to.be(undefined);
+        });
+    
+        it('throws error on undefined if required', function() {        
+            var phoneField = validators.telephoneField({required: true});
+            var tmp = phoneField.validate();
+            expect(tmp).to.not.be(undefined);
+        });
+        
+        it('throws error on malformed phonenumber', function() {        
+            var phoneField = validators.telephoneField({required: false});
+            var tmp = phoneField.validate("+4567$707 555 555");
+            expect(tmp).to.not.be(undefined);
+        });
+
+        it('throws error on too many characters', function() {        
+            var phoneField = validators.telephoneField({required: false});
+            var tmp = phoneField.validate("1234567890123456");
+            expect(tmp).to.not.be(undefined);
+        });
+    });
+
     
 });
