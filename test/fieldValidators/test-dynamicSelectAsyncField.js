fb83caa6d29cbede5567b59abffa875b2d6c8d94
jhsware <sebastian@urbantalk.se>
2016-12-12 13:00:53 +0100

    Reimplemented dynamic select so implementation is in field rather than utility (has a lot of advantages)

---
+++ test/fieldValidators/test-dynamicSelectAsyncField.js
@@ -0,0 +1,130 @@
+var assert = require('assert');
+var expect = require('expect.js');
+
+var createObjectPrototype = require('component-registry').createObjectPrototype
+var createInterface = require('component-registry').createInterface
+
+var validators = require('../../lib/field_validators');
+var DynamicSelectAsyncBaseField = require('../../lib').fieldObjectPrototypes.DynamicSelectAsyncBaseField;
+var Schema = require('../../lib/schema');
+var Promise = require('es6-promise');
+
+var IMyDynamicSelectAsyncField = createInterface({
+    name: 'IMyDynamicSelectAsyncField'
+})
+
+var dummyOptions = [{ name: 'one', title: 'The One'}, { name: 'two', title: 'Two'}, { name: 'three', title: 'Three'}]
+
+var MyDynamicSelectAsyncField = createObjectPrototype({
+    implements: [IMyDynamicSelectAsyncField],
+    extends: [DynamicSelectAsyncBaseField],
+
+    constructor: function (options) {
+        this._IDynamicSelectAsyncBaseField.constructor.call(this, options);
+        
+        this.valueType = validators.textField({required: true}); 
+    },
+
+    validateAsync: function (inp, options, context) {
+        var result = this._IDynamicSelectAsyncBaseField.validateAsync.call(this, inp);
+
+        // Check if we failed validation in DynamicSelectAsyncBaseField
+        if (result) return result;
+
+        return this.getOptionsAsync()
+            .then(function (options) {
+                var matches = false
+                for (var i = 0; i < options.length; i++) {
+                    if (options[i].name === inp) {
+                        matches = true
+                        break
+                    }
+                }
+            
+                if (!matches) {
+                    error = {
+                        type: 'constraint_error',
+                        message: "Valt värde finns inte i listan över tillåtna värden"
+                    }
+                    //console.log(error);
+                    return Promise.resolve(error);
+                } else {
+                    return Promise.resolve(undefined);
+                }
+            })
+    },
+
+    getOptionsAsync: function (inp, options, context) {
+        return Promise.resolve(dummyOptions)
+    },
+
+    getOptionTitleAsync: function (inp, options, context) {
+        for (var i = 0; i < dummyOptions.length; i++) {
+            if (dummyOptions[i].name === inp) {
+                return Promise.resolve(dummyOptions[i].title)
+            }
+        }
+        return Promise.resolve(undefined)
+    },
+
+    toFormattedString: function (inp) {
+        return this.valueType.fromString(inp);
+    },
+
+    fromString: function (inp) {
+        return this.valueType.fromString(inp);
+    }
+});
+
+var myDynamicSelectAsyncField = function (options) { return new MyDynamicSelectAsyncField(options) }
+
+describe('Dynamic select async field', function() {
+
+    it('allows you to select a value from the list', function(done) {        
+        var theField = myDynamicSelectAsyncField({
+            required: true
+        });
+    
+        var tmp = theField.validateAsync("one");
+        tmp.then(function (validationError) {
+            expect(validationError).to.be(undefined);
+            done();
+        })
+    });
+    it('throws an error if wrong type', function(done) {        
+        var theField = myDynamicSelectAsyncField({
+            required: true
+        });
+    
+        var tmp = theField.validateAsync("select-me");
+        tmp.then(function (validationError) {
+            expect(validationError).not.to.be(undefined);
+            done();
+        })
+    });
+
+    it('can convert a value to a title', function(done) {        
+        var theField = myDynamicSelectAsyncField({
+            required: true
+        });
+    
+        var tmp = theField.getOptionTitleAsync("one");
+        tmp.then(function (title) {
+            expect(title).to.equal('The One');
+            done();
+        })
+    });
+
+    it('convert a value to a title handles undefined', function(done) {        
+        var theField = myDynamicSelectAsyncField({
+            required: true
+        });
+    
+        var tmp = theField.getOptionTitleAsync("no exist");
+        tmp.then(function (title) {
+            expect(title).to.be(undefined);
+            done();
+        })
+
+    });
+});
